b8500c4729320a551e87d7ed62d0e600dd104dfe
Trung Do Van <trung.do@zalora.com>
2016-12-24 22:14:29 +0700

    100/100 Christmas String

--- hacker-dev/src/main/java/Main.java
+++ hacker-dev/src/main/java/Main.java
@@ -21,28 +21,51 @@ class Main {
 			names.add(scanner.next());
 	}
 
-	boolean match(int position, String second) {
-		//thay * vao vi tri position
-		for (int i = 0; i < second.length(); i++)
-			if ((i != position) && (s.charAt(i) != STAR) && (s.charAt(i) != second.charAt(i)))
-				return false;
-		return true;
-	}
-
-	int calculateNumMatch(int position) {
-		int result = 0;
-		for (String s : names)
-			if (match(position, s))
-				result++;
-		return result;
+	// 0..size: chi khac nhau duy nhat 1 cho tai do
+	// -1: giong het nhau
+	// -2: nhieu hon 2 cho khac nhau
+	int calculateDiffer(String first, String second) {
+		int result = -3; // chua co diem khac nhau
+		for (int i = 0; i < first.length(); i++) {
+			//neu co them 1 diem khac nhau
+			if (first.charAt(i) != STAR && first.charAt(i) != second.charAt(i)) {
+				// neu khong co diem khac nhau, thi result = i, co 1 vi tri khac nhau
+				if (result == -3) {
+					result = i;
+				} else if (result >= 0) {
+					//neu da co 1 diem khac nhau, return -2: co nhieu hon 2  cho khac nhau
+					return -2;
+				}
+			}
+		}
+		//khong co diem khac nhau
+		if (result == -3)
+			return -1;
+		else
+			// co duy nhat 1 diem khac nhau
+			return result;
 	}
 
 	void process() {
+		int[] differ = new int[n];
+		for (int i = 0; i < n; i++)
+			differ[i] = calculateDiffer(s, names.get(i));
+		if (debug) {
+			for (int i = 0; i < differ.length; i++)
+				System.out.print(", " + differ[i]);
+			System.out.println("----");
+		}
+
 		int max = 0;
-		for (int i = -1; i < s.length(); i++) {
-			int num = calculateNumMatch(i);
-			if (max < num)
-				max = num;
+		for (int i = 0; i < s.length(); i++) {
+			int match = 0;
+			for (int j = 0; j < n; j++) {
+				// neu giong het nhau, hoac khac nhau duy nhat tai diem i, thi match
+				if (differ[j] == -1 || differ[j] == i)
+					match++;
+			}
+			if (max < match)
+				max = match;
 		}
 		System.out.println(max);
 	}
@@ -52,6 +75,7 @@ class Main {
 		process();
 	}
 
+	static boolean debug = false;
 	public static void main(String args[] ) throws Exception {
 		new Main().run();
 	}
