db7e0ebffc1c53f7912b7d03bdcc5189f92a46e7
Trung Do Van <trung.do@zalora.com>
2016-12-27 01:05:10 +0700

    Two Robots: 11/15 (fail because of Timeout)

--- hacker-dev/src/main/java/Main.java
+++ hacker-dev/src/main/java/Main.java
@@ -1,35 +1,107 @@
 /**
- * Plus Minus
- * https://www.hackerrank.com/challenges/plus-minus
+ * Two Robot
+ * https://www.hackerrank.com/challenges/two-robots
  * point: 100/100
  */
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-	String format(float f){
-		return String.format("%.6f", f);
-	}
-	void run() {
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		int[] counts = new int[3];
-		for (int i = 0; i < n; i++) {
-			int value = scanner.nextInt();
-			if (value > 0)
-				counts[0]++;
-			else if (value < 0)
-				counts[1]++;
-			else
-				counts[2]++;
-		}
-		float tmp = n;
-		System.out.println( format(counts[0] / tmp));
-		System.out.println( format(counts[1] / tmp));
-		System.out.println( format(counts[2] / tmp));
+	class Point2D {
+		int x;
+		int y;
 
+		Point2D(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int hashCode() {
+			if (x <= y)
+				return x << 10 + y;
+			else
+				return y << 10 + x;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			Point2D point = (Point2D) obj;
+			return (this.x == point.x && this.y == point.y) || (this.x == point.y && this.y == point.x);
+		}
 	}
+
+	int t, m, n, result;
+	List<Point2D> moves;
+
+	int distance(int a, int b) {
+		if (a == 0)
+			return 0;
+		else
+			return Math.abs(a - b);
+	}
+
+	void putNew(Map<Point2D, Integer> next, Point2D point, int value) {
+		Integer oldValue = next.get(point);
+		if (oldValue == null)
+			next.put(point, value);
+		else {
+			if (oldValue > value)
+				next.put(point, value);
+		}
+	}
+
+	private Map<Point2D,Integer> calculateNext(Map<Point2D, Integer> current, Point2D move) {
+		Map<Point2D, Integer> next = new HashMap();
+		current.keySet().forEach( key -> {
+			int f = current.get(key);
+			// f(b, y) = f(x, y) + distance(x, a)
+			putNew(next, new Point2D(move.y, key.y), f + distance(key.x, move.x) );
+
+			// f(x, b) = f(x, y) + distance(y, a)
+			putNew(next, new Point2D(key.x, move.y), f + distance(key.y, move.x));
+		});
+		return next;
+	}
+
+	void processProblem(){
+		Map<Point2D, Integer> current;
+		current = new HashMap<Point2D, Integer>();
+
+		//init
+		current.put(new Point2D(0,0 ), 0);
+
+		for (int i = 0; i < n; i++) {
+			current = calculateNext(current, moves.get(i));
+		}
+		int min = Collections.min(current.values());
+		System.out.println(min + result);
+	}
+
+	void run() {
+
+		Scanner scan = new Scanner(System.in);
+		t = scan.nextInt();
+		for (int l1 = 0; l1 < t; l1++) {
+			m = scan.nextInt();
+			n = scan.nextInt();
+
+			//init
+			result = 0;
+			moves = new ArrayList<Point2D>(n);
+			for (int i = 0; i < n; i++) {
+				int a = scan.nextInt();
+				int b = scan.nextInt();
+				result += Math.abs(a - b);
+				moves.add(new Point2D(a, b));
+			}
+			processProblem();
+		}
+
+		scan.close();
+	}
+
 	public static void main(String args[]) throws Exception {
 		new Main().run();
 	}
