604b454c8021e06bf9605f837bae89f174790e20
Trung Do Van <trung.do@zalora.com>
2016-12-29 10:59:48 +0700

    #168: Number Rotation 3/10 Test #2: correct with m >= 12

---
+++ hacker-dev/src/main/java/Test2.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by trungdovan on 12/29/16.
+ */
+public class Test2 {
+}
---
+++ hacker-rank/Contest/Project Euler/0168_Number_Rotations.java
@@ -0,0 +1,67 @@
+/**
+ * Project Euler #168: Number Rotations
+ * link: https://www.hackerrank.com/contests/projecteuler/challenges/euler168
+ * point: 3/10
+ */
+
+/**
+ * test #2: m >= 12: correct
+ */
+
+/*
+ 102564
+ 128205
+ 142857
+ 153846
+ 179487
+ 205128
+ 230769
+
+ sum: 1142856
+ */
+
+import java.util.*;
+
+public class Main {
+	final int MOD = 100000;
+	final int fourfive = 45;
+	final int exceptions = 42856;
+
+	int mod(int value) {
+		return value % MOD;
+	}
+
+	int calculate(int n) {
+		if (n == 2)
+			return mod(11 * fourfive);
+		if (n == 3)
+			return mod(111 * fourfive);
+		if (n == 4)
+			return mod( 1111 * fourfive);
+		if (n == 5)
+			return mod(11111 * fourfive);
+		if (n % 6 != 0)
+			return mod(11111 * fourfive);
+		else
+		//if n % 6 == 0
+			return mod(11111 * fourfive + exceptions);
+	}
+
+	int process(int n) {
+		int sum = 0;
+		for (int i = 2; i <= n; i++) {
+			int valueI = calculate(i);
+			sum = mod(sum + valueI);
+		}
+		return sum;
+	}
+	void run() {
+		Scanner scanner = new Scanner(System.in);
+		int m = scanner.nextInt();
+		System.out.println(process(m));
+	}
+
+	public static void main(String[] args) {
+		new Main().run();
+	}
+}
