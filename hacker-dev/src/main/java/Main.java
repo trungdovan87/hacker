f53c526a38b34a0a3edb89797260091ed782d113
Trung Do Van <trung.do@zalora.com>
2016-12-29 20:39:58 +0700

    Euler #168: Number Rotations

--- hacker-dev/src/main/java/Main.java
+++ hacker-dev/src/main/java/Main.java
@@ -1,72 +1,60 @@
 /**
  * Project Euler #168: Number Rotations
- * link: https://www.hackerrank.com/contests/projecteuler/challenges/euler168
- * point: 3/10
+ * link: https://www.hackerrank.com/contests/projecteuler/challenges/euler168/submissions/code/34789148
+ * point: 100/100
  */
 
-/**
- * test #2: m >= 12: correct
- */
-
-/**
- Exception with 6
- 102564
- 128205
- 142857
- 153846
- 179487
- 205128
- 230769
-
- sum: 1142856
- ---------
-
- Exception with 6
- 1012658227848
-
- */
-
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
-	final int MOD = 100000;
-	final int fourfive = 45;
-	final int exceptions6 = 42856;
-	final int exceptions13 = 27848;
 
-	int mod(int value) {
-		return value % MOD;
+	BigInteger num0 = BigInteger.valueOf(0);
+	BigInteger num10 = BigInteger.valueOf(10);
+	BigInteger tenPower4 = BigInteger.valueOf(10000);
+
+	// A = [b(10 ^ (n-1) - x)] / (10x - 1)
+	BigInteger f(int n, int x, int b) {
+		BigInteger bigB = BigInteger.valueOf(b);
+		BigInteger bigX = BigInteger.valueOf(x);
+
+		BigInteger above = num10.pow(n - 1);
+		above = above.subtract(bigX);
+		above = above.multiply(bigB);
+
+		BigInteger below = BigInteger.valueOf(10 * x - 1);
+
+		if (above.mod(below).compareTo(num0) == 0) {
+			BigInteger tmp = above.divide(below);
+			if (tmp.toString().length() == n - 1)
+				return tmp;
+			else
+				return null;
+		} else {
+			return null;
+		}
 	}
 
-	int calculate(int n) {
-		if (n == 2)
-			return mod(11 * fourfive);
-		if (n == 3)
-			return mod(111 * fourfive);
-		if (n == 4)
-			return mod( 1111 * fourfive);
-		if (n == 5)
-			return mod(11111 * fourfive);
-		int result = mod(11111 * fourfive);
-		if (n % 6 == 0)
-			result = mod(result + exceptions6);
-		if (n % 13 == 0)
-			result = mod(result + exceptions13);
+	int process(int m) {
+		int result = 0;
+		for (int n = 2; n <= m; n++)
+			for (int x = 1; x <= 9; x++)
+				for (int b = 1; b <= 9; b++) {
+					BigInteger tmp = f(n, x, b);
+					if (tmp != null) {
+						//System.out.println(tmp.toString() + b);
+						int lt = tmp.mod(tenPower4).intValue() * 10 + b;
+						result = (result + lt) % 100000;
+					}
+				}
 		return result;
 	}
 
-	int process(int n) {
-		int sum = 0;
-		for (int i = 2; i <= n; i++) {
-			int valueI = calculate(i);
-			sum = mod(sum + valueI);
-		}
-		return sum;
-	}
 	void run() {
 		Scanner scanner = new Scanner(System.in);
 		int m = scanner.nextInt();
 		System.out.println(process(m));
+		scanner.close();
 	}
 
 	public static void main(String[] args) {
